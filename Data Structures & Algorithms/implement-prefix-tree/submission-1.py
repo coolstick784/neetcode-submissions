ab29113d9d3064a5147979614ea7c30997433427
Sahil Chugani <sahil.chugani50@gmail.com>
2026-05-23 18:50:30 -0400

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.py
@@ -0,0 +1,31 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.trees = {}
+
+    def insert(self, word: str) -> None:
+        prev = self.trees
+        for idx, ch in enumerate(word):
+            prev.setdefault(ch, {})
+            prev = prev[ch]
+            if idx == len(word) - 1:
+                prev[True] = word
+
+
+    def search(self, word: str) -> bool:
+        cur = self.trees
+        for ch in word:
+            cur = cur.get(ch, {})
+        if True in cur:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.trees
+        for ch in prefix:
+            if ch not in cur:
+                return False
+            cur = cur[ch]
+        return True
+        
+        
