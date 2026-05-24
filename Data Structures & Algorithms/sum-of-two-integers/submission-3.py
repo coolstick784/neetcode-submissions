025ea3d0ae69200cabf00779e8fe507b53f9e300
Sahil Chugani <sahil.chugani50@gmail.com>
2026-05-24 18:50:11 -0400

    Add: sum-of-two-integers - submission-3

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 2**32-1
+        mx = 2**31-1
+        while b != 0:
+
+            a, carry = (a^b) & (mask), (a&b) & (mask)
+            b = carry << 1
+        if abs(a) <= mx:
+            return a
+
+        return ~(a ^ mask)
