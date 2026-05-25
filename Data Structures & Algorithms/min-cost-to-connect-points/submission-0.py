54b3bf7120f0e68613f56c662cc4f34cada3c765
Sahil Chugani <sahil.chugani50@gmail.com>
2026-05-25 15:39:49 -0400

    Add: min-cost-to-connect-points - submission-0

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1:
+            return 0
+        # start with a point, then ask: what's the closest point to any of our current points that isn't a current point?
+
+        cur_points = set()
+        heap = []
+        self.res = 0
+        
+        def add_point(x, y):
+            print((x, y))
+            cur_points.add((x, y))
+            
+            for cx, cy in points:
+                if (cx, cy) in cur_points:
+                    continue 
+                heapq.heappush(heap, (abs(x-cx) + abs(y-cy), cx, cy))
+
+            if heap:
+                dist, cx, cy = heapq.heappop(heap)
+
+            while heap and (cx, cy) in cur_points:
+                dist, cx, cy = heapq.heappop(heap)
+            
+            
+            if (cx, cy) not in cur_points:
+
+                add_point(cx, cy)
+                self.res += dist
+
+        x, y = points[0]
+        add_point(x, y)
+        return self.res
