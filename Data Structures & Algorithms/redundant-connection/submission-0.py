cb36a58651c4d9319e44a59a2c009390c87406ad
Sahil Chugani <sahil.chugani50@gmail.com>
2026-05-25 16:08:22 -0400

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.py
@@ -0,0 +1,18 @@
+# an edge is necessary if 
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        union_find = [n for n in range(len(edges) + 1)]
+        
+        def trace(n):
+            if union_find[n] == n:
+                return n
+            val = trace(union_find[n])
+            n = val
+            return val
+        def union(n1, n2):
+            union_find[trace(n2)] = trace(n1)
+        
+        for start, end in edges:
+            if trace(start) == trace(end):
+                return [start, end]
+            union(start, end)
