839d8f26c051b82501dfbe011a3c2d5640e97e26
Sahil Chugani <sahil.chugani50@gmail.com>
2026-05-28 09:54:53 -0400

    Add: word-break-ii - submission-0

---
+++ Data Structures & Algorithms/word-break-ii/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        trie = {}
+        for word in wordDict:
+            cur = trie
+            for idx, ch in enumerate(word):
+                cur.setdefault(ch, {})
+                if idx == len(word) - 1:
+                    cur[ch][True] = word
+                cur = cur[ch]
+        def solve(start):
+            if start == len(s):
+                return [[]]
+            out = []
+            idx = start
+            chs = []
+            cur = trie
+            while idx < len(s):
+                ch = s[idx]
+                if cur.get(ch) is not None:
+                    cur = cur[ch]
+                    chs.append(ch)
+                    if True in cur:
+ 
+                        out.extend([["".join(chs)] + sol for sol in solve(idx+1)])
+                    idx += 1
+                else:
+                    break
+           
+            return out
+
+
+
+        res = solve(0)
+        return [" ".join(l) for l in res]
